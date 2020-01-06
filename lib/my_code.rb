bae353091c5850915eef8f41b569bc0298de4e33
nickie kane <kameo712@gmail.com>
2020-01-06 01:38:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,7 +50,7 @@ def reduce_to_total(source_array, starting_point)
   if starting_point != nil 
     n_sum = starting_point
   end
-  n_sum = nil
+  n_sum = 0
   count = 0
   while count < source_array.length
       n_sum += source_array[count]
